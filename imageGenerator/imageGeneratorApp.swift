ac03292762e91f7bdc381e9165b92f64e4a55c3d
bekoszn <berkeozguder@gmail.com>
2024-07-22 19:52:30 +0300

    Initial Commit

---
+++ imageGenerator/imageGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  imageGeneratorApp.swift
+//  imageGenerator
+//
+//  Created by Berke Özgüder on 22.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct imageGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
